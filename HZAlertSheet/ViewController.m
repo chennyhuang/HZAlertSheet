57cd3e45a5a3aa35976f10d18ce709065be1cbba
chennyhuang <huangzhenyu!5>
2018-07-06 15:57:07 +0800

    上传gif

--- HZAlertSheet/ViewController.m
+++ HZAlertSheet/ViewController.m
@@ -10,6 +10,7 @@
 #import "HZAlertSheet.h"
 #define HZPageControlColor(c) [UIColor colorWithRed:((c>>16)&0xFF)/255.0 green:((c>>8)&0xFF)/255.0 blue:(c&0xFF)/255.0 alpha:1.0]
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
@@ -26,7 +27,7 @@
 }
 
 - (IBAction)pop:(id)sender {
-    HZAlertSheet *alertSheet = [[HZAlertSheet alloc] initWithMessage:@"11111111111111111111111111111111112222222222222222222222222222222222222" choiceButtonTitles:@[@"男", @"女"]];
+    HZAlertSheet *alertSheet = [[HZAlertSheet alloc] initWithMessage:@"选择性别" choiceButtonTitles:@[@"男", @"女"]];
     /*
         [alertSheet setMessageStyleWith:@{NSFontAttributeName:[UIFont systemFontOfSize:10.0f],NSForegroundColorAttributeName:HZPageControlColor(0x666666)}];
         [alertSheet setItemStyleWith:@{NSFontAttributeName:[UIFont systemFontOfSize:30.0f],NSForegroundColorAttributeName:[UIColor colorWithRed:84/255.0 green:174/255.0 blue:247/255.0 alpha:1.0]} index:0];
@@ -38,11 +39,13 @@
             case 0:
             {
                 NSLog(@"我选择了男");
+                self.label.text = @"男";
             }
                 break;
             case 1:
             {
                 NSLog(@"我选择了女");
+                self.label.text = @"女";
             }
                 break;
             default:
